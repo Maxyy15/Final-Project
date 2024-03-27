edf22046706958c9907c9f827a9b562c8b63d741
Maxy15 <maxwell_clement@outlook.com>
2024-03-27 11:49:27 -0400

    Added several classes, code to be added
    Areas, Monsters (As well as monster types), Player, Weapons, Magic, Armor, and several other classes

---
+++ Final Project/Important Stuff/Game.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Project
+{
+    internal class Game
+    {
+        List<Areas> areas = new List<Areas>();
+        Areas currentArea;
+        internal bool IsGameOver() => isFinished;
+        static bool isFinished;
+        static string nextArea = "";
+
+        internal void Add(Areas area)
+        {
+            areas.Add(area);
+            if (currentArea == null)
+            {
+                currentArea = area;
+            }
+        }
+
+        internal string CurrentAreaDescription => currentArea.CreateDescription();
+
+        internal void ReceiveChoice(string choice)
+        {
+            currentArea.ReceiveChoice(choice);
+            CheckTransition();
+        }
+
+        internal static void Transition<T>() where T : Areas
+        {
+            nextArea = typeof(T).Name;
+        }
+
+        internal static void Finish()
+        {
+            isFinished = true;
+        }
+
+        internal void CheckTransition()
+        {
+            foreach (var area in areas)
+            {
+                if (area.GetType().Name == nextArea)
+                {
+                    nextArea = "";
+                    currentArea = area;
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Final Project/Important Stuff/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var game = new Game();
+            game.Add(new Bedroom());
+            game.Add(new Bathroom());
+            game.Add(new AtticRoom());
+            
+            while (!game.IsGameOver())
+            {
+                Console.WriteLine("--");
+                Console.WriteLine(game.CurrentAreaDescription);
+                string choice = Console.ReadLine().ToLower() ?? "";
+                Console.Clear();
+                game.ReceiveChoice(choice);
+            }
+
+                Console.WriteLine("END");
+                Console.ReadLine();
+        }
+    }
+}
